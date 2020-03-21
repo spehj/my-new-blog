52d8f8e4c52675a13a2856f6f4efc90112574405
jspeh <jurespeh10@gmail.com>
2020-03-21 14:37:53 +0100

    my-new-blog, second commit

--- django_project/settings.py
+++ django_project/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'crispy_forms',
-    'phonenumber_field',
     'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
